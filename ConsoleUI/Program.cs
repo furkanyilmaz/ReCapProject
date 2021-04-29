0771b8a9e40c18cdd40c3125de7c9f6c0575a112
Furkan <furkanyilmaz-@hotmail.com>
2021-04-29 15:01:09 +0300

    car_adding

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -6,47 +6,57 @@ using System;
 
 namespace ConsoleUI
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            //CarManager carManager = new CarManager(new EfCarDal());
+            Car car1 = new Car();
+
+            Console.WriteLine("car id:");
+            car1.Id = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("car brand id:");
+            car1.BrandId = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("car color id:");
+            car1.ColorId = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("car model year: ");
+            car1.ModelYear = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("car daily price :");
+            car1.DailyPrice = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("car model name: ");
+            car1.Description = (Console.ReadLine());
+
+            CarTest(car1);
+
+           // BrandTest();
+
+        }
+
+        //private static void BrandTest()
+        //{
+        //    BrandManager brandManager = new BrandManager(new EfBrandDal());
+        //    foreach (var brand in brandManager.GetAll())
+        //    {
+        //        Console.WriteLine(brand.BrandName);
+        //    }
+        //}
+
+        private static void CarTest(Car car1)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
 
 
 
-            //Car car1 = new Car();
+            carManager.Add(car1);
 
-            //Console.WriteLine("car id:");
-            //car1.Id = Convert.ToInt32(Console.ReadLine());
-
-            //Console.WriteLine("car brand id:");
-            //car1.BrandId = Convert.ToInt32(Console.ReadLine());
-
-            //Console.WriteLine("car color id:");
-            //car1.ColorId = Convert.ToInt32(Console.ReadLine());
-
-            //Console.WriteLine("car model year: ");
-            //car1.ModelYear = Convert.ToInt32(Console.ReadLine());
-
-            //Console.WriteLine("car daily price :");
-            //car1.DailyPrice = Convert.ToInt32(Console.ReadLine());
-
-            //Console.WriteLine("car model name: ");
-            //car1.Description =(Console.ReadLine());
-
-
-
-
-
-            //carManager.Add(car1);
-
-            //foreach (var car in carManager.GetAll())
-            //{
-            //    Console.WriteLine(car.Description);
-            //}
-
-            
-
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
         }
     }
 }
